48305b35d06f6782385517e743e21ee015e11c5d
king-pan <pwpw1218@gmail.com>
2018-09-02 23:02:22 +0800

    mint-ui的使用

---
+++ src-mint-ui/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <mt-button type="primary"  @click.native="handleClick" style="width:100%">primary</mt-button>
+</template>
+
+<script>
+  import {Toast} from 'mint-ui'
+  export default {
+    name: 'App',
+    data(){
+      return{
+
+      }
+    },
+    methods:{
+      handleClick(){
+        Toast('提示信息')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
